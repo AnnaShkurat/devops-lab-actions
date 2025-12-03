85a6e502e9d7e6dd4054286d8a4448652258be20
AnnaShkurat <135139395+AnnaShkurat@users.noreply.github.com>
2025-12-03 13:24:20 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,8 +2,8 @@ import pytest
 from app import add, subtract
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
+    assert add(-1, 0) == -1
 
 def test_subtract():
+    assert subtract(9, 3) == 6
     assert subtract(5, 3) == 2
